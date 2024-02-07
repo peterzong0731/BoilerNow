28ce04fe2c3337cdd1b1c3ebd3dfb3fef758fb0c
unknown <ni95@purdue.edu>
2024-02-07 15:24:49 -0500

    Post Card

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import PostCard from './PostCard'
+import './Posts.css'
 
 function Posts() {
   return (
-    <div>Posts</div>
+    <div className="posts-outer-container">
+      <div className="posts-inner-container">
+        <PostCard />
+        <PostCard />
+      </div>
+    </div>
   )
 }
 

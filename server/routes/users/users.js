5fa12ab64f4456d5b4eeb954e0a542b5762a8dcf
yashnarain <agarw236@purdue.edu>
2024-02-15 16:38:40 -0500

    removed for sanity

--- server/routes/users/users.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import fs from "fs";
-import db from "../../conn.js";
-
-const router = express.Router();
-const newUserTemplate = fs.readFileSync("./routes/users/dbTemplates/newUserTemplate.json", "utf8");
-
-export default router;

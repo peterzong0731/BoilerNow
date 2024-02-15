71dbc510c6085da75ba3b6db43410dda5ad37ed1
yashnarain <agarw236@purdue.edu>
2024-02-15 16:22:04 -0500

    added google oauth

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,12 @@ import fs from "fs";
 import md5 from "md5";
 import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
+import passport from 'passport';
+import OAuth from 'passport-google-oauth20';
+import session from "express-session";
 
 const router = express.Router();
+const GoogleStrategy = OAuth.Strategy;
 const newUserTemplate = fs.readFileSync("./routes/newUserTemplate.json", "utf8");
 const transporter = nodemailer.createTransport({
 	service: 'gmail',
@@ -17,6 +21,39 @@ const transporter = nodemailer.createTransport({
 		pass: process.env.APP_PASS,
 	}
 });
+
+router.use(
+	session({
+		secret: "TOPSECRETWORD",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get("/home", (req, res) => {
+	// add !req.session.user && 
+	if (!req.isAuthenticated()) {
+		res.redirect("/login")
+		console.log('Log In First');
+	} else {
+		// res.render("home.ejs");
+		console.log('Landed');
+	}
+});
+
+router.get("/auth/google",
+	passport.authenticate('google', { scope: ["profile", "email"] })
+);
+
+router.get("/auth/google/boilernow",
+	passport.authenticate('google', { failureRedirect: "http://localhost:8000/login" }),
+	function (req, res, err) {
+		console.log("google login")
+		console.log(res.data)
+		res.redirect("http://localhost:8000/home");
+	});
 
 router.get('/', async (req, res) => {
 	// test connection to database
@@ -80,17 +117,17 @@ router.post('/register', async function (req, res) {
 	if (req.body.email.endsWith('@purdue.edu')) {
 		const link = `http://localhost:${process.env.PORT}/verify-user/${req.body.username}/${req.body.email}/${md5(req.body.password)}`;
 		const msg = {
-		  from: '"Team BoilerNow" boilernow2023@gmail.com',
-		  to: req.body.email,
-		  subject: 'BoilerNow Email Verification',
-		  text: `Hello from BoilerNow! Boiler Up! Please click the link to verify your email:\n${link}.\n`
+			from: '"Team BoilerNow" boilernow2023@gmail.com',
+			to: req.body.email,
+			subject: 'BoilerNow Email Verification',
+			text: `Hello from BoilerNow! Boiler Up! Please click the link to verify your email:\n${link}.\n`
 		}
-		transporter.sendMail(msg, function(error, info){
-		  if (error) {
-			console.log(error);
-		  } else {
-			console.log('Email sent: ' + info.response);
-		  }
+		transporter.sendMail(msg, function (error, info) {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log('Email sent: ' + info.response);
+			}
 		});
 		// TODO: Render Home Page
 		return;
@@ -109,7 +146,7 @@ router.post('/register', async function (req, res) {
 	}
 });
 
-router.get("/verify-user/:name/:email/:password", async function(req, res) {
+router.get("/verify-user/:name/:email/:password", async function (req, res) {
 	console.log("User Successfully Verified!");
 
 	const { name, email, password } = req.params;
@@ -144,7 +181,7 @@ router.get("/verify-user/:name/:email/:password", async function(req, res) {
 		console.log(e);
 		res.status(500).send("Error creating new user");
 	}
-  });
+});
 
 // POST Route for Login
 router.post('/login', async function (req, res) {
@@ -262,6 +299,55 @@ router.post("/reset-password", async function (req, res) {
 		.catch(err => {
 			console.error(`Failed to update password: ${err}`);
 		});
+});
+
+passport.use(
+	"google",
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: "http://localhost:8000/auth/google/boilernow",
+			userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+		},
+		async (accessToken, refreshToken, profile, cb) => {
+			try {
+				console.log(profile);
+				let jsonObj = JSON.parse(newUserTemplate);
+				jsonObj.createdDateTime = new Date();
+				jsonObj.name = profile.displayName;
+				jsonObj.email = profile.emails[0].value;
+				jsonObj.googleId = profile.id;
+				console.log(jsonObj);
+
+				const user = await db
+					.collection("users")
+					.findOne({ email: jsonObj.email });
+				if (user != null) {
+					console.log('User already exists');
+					return cb(null, user);
+				}
+
+				// Insert new document to users collection
+				const newUser = await db
+					.collection("users")
+					.insertOne(jsonObj);
+				console.log("Inserted new user with _id: " + newUser['insertedId']);
+
+				return cb(null, newUser);
+			} catch (err) {
+				return cb(err);
+			}
+		}
+	)
+);
+
+passport.serializeUser((user, cb) => {
+	cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+	cb(null, user);
 });
 
 export default router;
